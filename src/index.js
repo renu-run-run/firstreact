c4bfc03f2f3403d399d4514330f24166dc9f98ad
Renuka Choudhary <renurunrun@gmail.com>
2022-02-13 19:46:45 +0530

    my first commit reactweb

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+  <h1>Lata Mangeshkar</h1>,
+    <img src="https://www.vibesofindia.com/wp-content/uploads/2022/02/Lata-Mangeshkar-1.jpeg" /> 
+    <ul>
+  <li>Lata Mangeshkar recorded songs in over thirty-six Indian languages and a few foreign languages</li>
+  <li>She sang the song "Naachu Yaa Gade, Khelu Saari Mani Haus Bhaari"</li>
+  <li>She sang many raga-based songs for Naushad in movies such as Deedar (1951)</li>
+  <li>Mangeshkar's song "Pyar Kiya To Darna Kya" from Mughal-e-Azam (1960)</li>
+  <li>She had sung in R. D. Burman's first film, Chhote Nawab (1961)</li>
+  <li>She sang many raga-based songs for Naushad in movies such as Deedar (1951)</li>
+  </ul>
+    
+  </>,
+    
+  
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
